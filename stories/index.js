1084ef9e486458a9efc72025be09b09fac19cb9d
William Durand <will+git@drnd.me>
2018-03-20 16:02:31 +0100

    Add more datasets and add more stories

--- stories/index.js
+++ stories/index.js
@@ -5,8 +5,10 @@ import { withNotes } from '@storybook/addon-notes';
 import { tablify } from 'html-tablify';
 
 import OncoPrint from '../src';
-import partialMutations from '../tests/fixtures/partial-mutations';
-import fullMutations from '../tests/fixtures/full-mutations';
+import cBioPortalData from '../tests/fixtures/cbioportal-data';
+import dataset1 from '../tests/fixtures/dataset-1';
+import dataset2 from '../tests/fixtures/dataset-2';
+import dataset3 from '../tests/fixtures/dataset-3';
 
 const createNotes = (data) => {
   const table = tablify({ data, pretty: false, cellpadding: 5 });
@@ -14,21 +16,26 @@ const createNotes = (data) => {
   return `<h2>Input Mutation Data</h2>${table}`;
 };
 
-const partialNotes = createNotes(partialMutations);
-const fullNotes = createNotes(fullMutations);
-
 storiesOf('OncoPrint', module)
   .add(
     'with a few entries',
-    withNotes(partialNotes)(() => <OncoPrint data={partialMutations} />)
+    withNotes(createNotes(dataset1))(() => <OncoPrint data={dataset1} />)
   )
   .add(
-    'with a few entries (full width)',
-    withNotes(partialNotes)(() => (
-      <OncoPrint data={partialMutations} fullWidth />
+    'with a few more entries (full width)',
+    withNotes(createNotes(dataset2))(() => (
+      <OncoPrint data={dataset2} fullWidth />
+    ))
+  )
+  .add(
+    'with two genes and lots of truncating mutations',
+    withNotes(createNotes(dataset3))(() => (
+      <OncoPrint data={dataset3} fullWidth />
     ))
   )
   .add(
     'with cBioPortal test data (full width)',
-    withNotes(fullNotes)(() => <OncoPrint data={fullMutations} fullWidth />)
+    withNotes(createNotes(cBioPortalData))(() => (
+      <OncoPrint data={cBioPortalData} fullWidth />
+    ))
   );
